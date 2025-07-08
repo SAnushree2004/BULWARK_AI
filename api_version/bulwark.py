d69db8d8e3a15b91ed13a919a08a833a684f9d8f
S Anushree <119684668+SAnushree2004@users.noreply.github.com>
2025-07-08 10:20:16 +0530

    Updated version route for Render

--- api_version/bulwark.py
+++ api_version/bulwark.py
@@ -527,4 +527,5 @@ def check_token_expiry():
         return jsonify({"error": str(e), "expired": True}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
